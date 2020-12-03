e6d3c307cfdfc74fa5118aefa4e93efe370ef340
caofanghui <caofanghui@bytedance.com>
2020-12-03 18:54:51 +0800

    update.

--- src/views/elementUi/Button.vue
+++ src/views/elementUi/Button.vue
@@ -8,6 +8,47 @@
       <el-button type="info">信息按钮</el-button>
       <el-button type="warning">警告按钮</el-button>
       <el-button type="danger">危险按钮</el-button>
+      <el-link
+        class="link"
+        type="primary"
+        href="https://element.eleme.io/#/zh-CN/component/button"
+        target="_blank"
+      >
+        elementui的button地址
+      </el-link>
+    </div>
+
+    <div class="icon-part">
+      <Title v-title="'icon'"></Title>
+      <i class="el-icon-edit"></i>
+      <i class="el-icon-share"></i>
+      <i class="el-icon-platform-eleme"></i>
+      <i class="el-icon-eleme"></i>
+      <i class="el-icon-delete-solid"></i>
+      <i class="el-icon-delete"></i>
+      <i class="el-icon-s-tools"></i>
+      <i class="el-icon-setting"></i>
+      <el-link
+        class="link"
+        type="primary"
+        href="https://element.eleme.io/#/zh-CN/component/icon"
+        target="_blank"
+      >
+        elementui的icon地址
+      </el-link>
+    </div>
+
+    <div class="link-part">
+      <Title v-title="'link'"></Title>
+      <el-link href="javascript:;"> 默认链接 </el-link>
+      <el-link
+        class="link"
+        type="primary"
+        href="https://element.eleme.io/#/zh-CN/component/link"
+        target="_blank"
+      >
+        elementui的link地址
+      </el-link>
     </div>
   </div>
 </template>
@@ -31,5 +72,16 @@ export default {
 
 <style lang="scss" scoped>
 .element-button {
+  .icon-part {
+    /deep/ i {
+      font-size: 20px;
+      margin: 0 10px 10px 0;
+    }
+  }
+
+  .link {
+    margin-left: 50px;
+    font-size: 18px;
+  }
 }
 </style>
